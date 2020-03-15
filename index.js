8caf889ae72774cd84a8682ca0567aea124513eb
determined-machine-0888 <luzybry94@gmail.com>
2020-03-15 22:53:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(array) {
+  let str = ""
+  for(let i = 0; i < array.length; i++) {
+  str += `${i+1}. ${array[i]} `
+  }
+  return str
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(array) {
+  let newArray = []
+  for(let i = 0; i < array.length; i++) {
+    newArray.push(`${array[i].toUpperCase()}!`)
+  }
+  
+  return newArray
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(array) {
+  for(let i = 0; i < array.length; i++) {
+    if (array[i].length > 4) {
+      return true
+    } else {
+      return false
+      }
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese(snacksArray) {
+  let cheeses = ['cheddar', 'gouda', 'camembert']
+  for(let i = 0; i < snacksArray.length; i++) {
+    
+    for(let j = 0; j < cheeses.length; j++) {
+      console.log(snacksArray[i])
+      console.log(cheeses[j])
+      if (snacksArray[i] == cheeses[j]) {
+        return cheeses[j]
+      } 
+        
+      
+      
+    }
+  }
+  return 'no cheese!'
 }
+
